a8dbf531748a006b60a07a13fbe2e06eb285df20
Joshua Moody <joshuajmoody@gmail.com>
2014-07-02 17:36:02 +0200

    clean gems needs to guard against incompat rubygems version

--- scripts/clean-gems.rb
+++ scripts/clean-gems.rb
@@ -1,5 +1,15 @@
 #!/usr/bin/env ruby
 
+rubygems_version = `gem --version`.strip!
+
+if rubygems_version != '2.3.0'
+  puts "FAIL: found rubygems gem version '#{rubygems_version}'"
+  puts "FAIL: please run"
+  puts "FAIL:   $ gem system --update"
+  puts "FAIL: and rerun this command"
+  exit 1
+end
+
 raw = `gem list --no-version`
 gems = raw.split("\n")
 
@@ -12,14 +22,17 @@ gems.each do |gem|
   if gem == 'bundler'
     skipped_bundler = true
   elsif excluded.include?(gem)
-    skipped_msgs << "#{gem}\n"
+    skipped_msgs << "* #{gem}\n"
   else
     system "gem uninstall -Vax --force --no-abort-on-dependent #{gem}"
   end
 end
 
+puts ''
 puts '=== INFO ==='
-puts 'skipped the following default gems'
+puts ''
+puts 'skipped the following default gems which cannot be uninstalled'
+puts ''
 puts skipped_msgs
 
 if skipped_bundler
