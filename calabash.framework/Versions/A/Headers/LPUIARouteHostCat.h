53cbc4d1d61982173bb6844516e1482d471c9679
Joshua Moody <joshuajmoody@gmail.com>
2014-07-30 01:32:45 +0200

    WIP updated calabash.framework to 0.10.0.pre4 - HOST CAT

---
+++ calabash.framework/Versions/A/Headers/LPUIARouteHostCat.h
@@ -0,0 +1,8 @@
+//  Created by Karl Krukow on 08/04/12.
+//  Copyright (c) 2012 LessPainful. All rights reserved.
+#import <Foundation/Foundation.h>
+#import "LPRoute.h"
+
+@interface LPUIARouteHostCat : NSObject<LPRoute>
+
+@end
