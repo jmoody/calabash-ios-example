ea9fb2ce015d65ea216750143625d0480f15c744
Joshua Moody <joshuajmoody@gmail.com>
2014-07-30 18:47:47 +0200

    WIP importing UIAutomation.h

--- calabash.framework/Versions/A/Headers/LPUIARouteHostCat.h
+++
@@ -1,8 +0,0 @@
-//  Created by Karl Krukow on 08/04/12.
-//  Copyright (c) 2012 LessPainful. All rights reserved.
-#import <Foundation/Foundation.h>
-#import "LPRoute.h"
-
-@interface LPUIARouteHostCat : NSObject<LPRoute>
-
-@end
