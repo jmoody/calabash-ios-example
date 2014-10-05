ed271a7dcfa96a076143a5a2e5b5c909e8ec8ca8
Joshua Moody <joshuajmoody@gmail.com>
2014-10-05 10:22:08 -0400

    Update the launch hooks

--- features/support/01_launch.rb
+++ features/support/01_launch.rb
@@ -29,28 +29,41 @@ module LaunchControl
   end
 end
 
+Before('@reset_app_before_hook') do
+  ENV['RESET_BETWEEN_SCENARIOS'] = '1'
+end
+
+Before('@reset_simulator_before_hook') do
+  launcher = LaunchControl.launcher
+  if launcher.simulator_target?
+    launcher.reset_simulator
+  elsif xamarin_test_cloud?
+    ENV['RESET_BETWEEN_SCENARIOS'] = '1'
+  else
+    # no-op for devices
+  end
+end
 
 Before do |scenario|
   launcher = LaunchControl.launcher
   unless launcher.calabash_no_launch?
-    launcher.relaunch({:launch_retries => 1})
+    launcher.relaunch({:launch_retries => 2})
     launcher.calabash_notify(self)
   end
+  ENV['RESET_BETWEEN_SCENARIOS'] = '0'
 end
 
 After do |scenario|
   launcher = LaunchControl.launcher
-  unless launcher.calabash_no_stop?
-    calabash_exit
-    if launcher.active?
-      launcher.stop
-    end
+  calabash_exit
+  if launcher.active?
+    launcher.stop
   end
 end
 
 at_exit do
   launcher = LaunchControl.launcher
   if launcher.simulator_target?
-    Calabash::Cucumber::SimulatorHelper.stop unless launcher.calabash_no_stop?
+    launcher.simulator_launcher.stop unless launcher.calabash_no_stop?
   end
 end
